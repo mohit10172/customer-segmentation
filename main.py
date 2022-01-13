2941e205aa983ec381c22fa7c9fe3dc292f95f90
Mohit <mohit.dhere1106@gmail.com>
2022-01-13 20:33:02 +0530

    main.py modified

--- main.py
+++ main.py
@@ -3,17 +3,34 @@ import pandas as pd
 import numpy as np
 from sklearn.cluster import KMeans
 import matplotlib.pyplot as plt
+import seaborn as sns
 
 
 train_data = pd.read_csv("E:\Customer Segmentation Project\dataset\Mall_Customers.csv")
 customerId = train_data.pop('CustomerID')
 #print(train_data.head(),"\n",train_data.describe())
 
-train_data.Gender.value_counts().plot(kind='bar')
+sns.countplot(x='Gender', data=train_data)
+plt.title('Distribution Of Gender')
 plt.show()
 
-plt.scatter(train_data['Age'], train_data['Annual Income (k$)'], s=50, c='blue')
-plt.xlabel('Age')
-plt.ylabel('Annual Income (k$)')
-plt.title("Age vs Annual Income (k$)")
+plt.scatter(train_data['Annual Income (k$)'], train_data['Spending Score (1-100)'], s=50, c='blue')
+plt.xlabel('Annual Income (k$)')
+plt.ylabel('Spending Score (1-100)')
+plt.title("Annual Income (k$) vs Spending Score (1-100)")
+plt.show()
+
+
+X = train_data[['Annual Income (k$)', 'Spending Score (1-100)']]
+wcss=[]
+for i in range(1,11):
+    kmeans = KMeans(n_clusters=i)
+    kmeans.fit(X)
+    wcss.append(kmeans.inertia_)
+
+plt.plot(range(1,11),wcss)
+plt.plot(range(1,11),wcss, linewidth=2, color="red", marker ="8")
+plt.xlabel("K Value")
+plt.xticks(np.arange(1,11,1))
+plt.ylabel("WCSS")
 plt.show()
