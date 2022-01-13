e8e80c296803173402b9e1809982bdca87dea666
Mohit <mohit.dhere1106@gmail.com>
2022-01-13 13:40:45 +0530

    main.py modified

--- main.py
+++ main.py
@@ -7,7 +7,13 @@ import matplotlib.pyplot as plt
 
 train_data = pd.read_csv("E:\Customer Segmentation Project\dataset\Mall_Customers.csv")
 customerId = train_data.pop('CustomerID')
-print(train_data.head())
+#print(train_data.head(),"\n",train_data.describe())
 
-plt.scatter(train_data[:,0], train_data[:,1], s=50, c='b')
+train_data.Gender.value_counts().plot(kind='bar')
+plt.show()
 
+plt.scatter(train_data['Age'], train_data['Annual Income (k$)'], s=50, c='blue')
+plt.xlabel('Age')
+plt.ylabel('Annual Income (k$)')
+plt.title("Age vs Annual Income (k$)")
+plt.show()
